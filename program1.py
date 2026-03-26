5257bebcc6487f7169d84b9664a0b9684b57d789
prudhvinathreddypalle <prudhvinathreddypallebsc24@rvu.edu.in>
2026-03-26 15:04:22 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,8 +1,11 @@
 n = int(input("Enter number of elements: "))
 arr = list(map(int, input("Enter elements: ").split()))
 
-for i in range(0, n - 1, 2):
-    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+if len(arr) != n:
+    print("Error: Number of elements doesn't match n")
+else:
+    for i in range(0, n - 1, 2):
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
-print("Array after swapping adjacent elements:")
-print(*arr)
+    print("Array after swapping adjacent elements:")
+    print(*arr)
